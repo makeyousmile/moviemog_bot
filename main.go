d59c10f9e7cdfbe75beca0014ba426a2ab746be6
makeyousmile <denis.zarubin@gmail.com>
2017-10-03 12:43:39 +0300

    init

--- main.go
+++ main.go
@@ -1,1 +1,32 @@
-package moviemog_bot
+package main
+
+import (
+	"github.com/Syfaro/telegram-bot-api"
+	"log"
+)
+
+func main()  {
+	bot, err := tgbotapi.NewBotAPI("437757616:AAErig4Hb9ZZoVhS5CnTUPaI4DbsCLl5Q3E")
+	if err != nil{
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+	var ucfg tgbotapi.UpdateConfig = tgbotapi.NewUpdate(0)
+	ucfg.Timeout = 60
+	updates, err := bot.GetUpdatesChan(ucfg)
+
+	for update := range updates{
+		if update.Message == nil{
+			continue
+		}
+
+		log.Printf("[%s]%s", update.Message.From.FirstName,update.Message.Text)
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		bot.Send(msg)
+	}
+
+}
--- parser.go
+++ parser.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"github.com/PuerkitoBio/goquery"
+	"log"
+	"fmt"
+)
+
+func getMovies() string {
+	doc, err := goquery.NewDocument("http://afisha.tut.by/film-mogilev/")
+	if err != nil {
+		log.Fatal(err)
+	}
+	var out string = "aloha"
+	// Find the review items
+	doc.Find(".events-block js-cut_wrapper").Each(func(i int, s *goquery.Selection) {
+		// For each item found, get the band and title
+		band := s.Find("a").Text()
+		title := s.Find("i").Text()
+		fmt.Printf("Review %d: %s - %s\n", i, band, title)
+		out = band
+	})
+	return out
+}
